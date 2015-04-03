2697b3f71c774317e3f6d21387b3831f4aa9dfea
bugisup <bugiz300@yandex.ru>
2015-04-03 13:42:54 +0300

    File update

--- admin.php
+++ admin.php
@@ -10,7 +10,7 @@ if (empty($_SESSION['mail'])){
 }
 
 if (isset($_POST))
-    //TODO: AI XSS issue #7, Medium, Cross-site Scripting, https://github.com/userwithrepo/ACSI_S4/issues/7
+    //TODO: AI XSS issue #8, Medium, Cross-site Scripting, https://github.com/userwithrepo/ACSI_S4/issues/8
     var_dump($_POST);
 
 
