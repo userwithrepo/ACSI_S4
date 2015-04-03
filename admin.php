4c5779739c620fb6fca27d139520d7e5cf8cf675
userwithrepo <bugiz300@yandex.com>
2015-04-03 17:22:13 +0300

    Update admin.php

--- admin.php
+++ admin.php
@@ -11,19 +11,12 @@ if (empty($_SESSION['mail'])){
 
 if (isset($_POST))
 {
-    //FP: AI XSS issue #39, Medium, Cross-site Scripting, https://github.com/userwithrepo/ACSI_S4/issues/39, ed0ff580
     echo $_POST['v'];
-    //VERIFY: AI XSS issue #39, Medium, Cross-site Scripting, https://github.com/userwithrepo/ACSI_S4/issues/39, 0b2bb647
     echo str_replace($_POST['a'], "\"", "");
-    //TODO: AI XSS issue #39, Medium, Cross-site Scripting, https://github.com/userwithrepo/ACSI_S4/issues/39, 453477dc
     echo $_POST['b'];
-    //TODO: AI XSS issue #39, Medium, Cross-site Scripting, https://github.com/userwithrepo/ACSI_S4/issues/39, 585ce7ca
     echo $_POST['c'];
-    //TODO: AI XSS issue #39, Medium, Cross-site Scripting, https://github.com/userwithrepo/ACSI_S4/issues/39, 344b1248
     echo $_POST['d'];
-    //TODO: AI XSS issue #39, Medium, Cross-site Scripting, https://github.com/userwithrepo/ACSI_S4/issues/39, c0dbc9fd
     echo $_POST['e'];
-    //TODO: AI XSS issue #39, Medium, Cross-site Scripting, https://github.com/userwithrepo/ACSI_S4/issues/39, 4f31cd61
     echo $_POST['f'];
 }
 
