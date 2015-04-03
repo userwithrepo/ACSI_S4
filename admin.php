0cd89d7b79767036d91c9bb0e11a0cd7dbe6a50c
userwithrepo <bugiz300@yandex.com>
2015-04-03 15:09:11 +0300

    Update admin.php

--- admin.php
+++ admin.php
@@ -11,10 +11,9 @@ if (empty($_SESSION['mail'])){
 
 if (isset($_POST['v']))
 {
-    $val = $_POST['v'];
-    $val = replace($val, "'", "");
+    $val = $_POST['v']."text";
     //VERIFY: AI XSS issue #19, Medium, Cross-site Scripting, https://github.com/userwithrepo/ACSI_S4/issues/19
-    var_dump($val);
+    echo $val;
 }
 
 
