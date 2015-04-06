a94736cfcd30fee67713439dcf374884c2cbf4bd
bugisup <bugiz300@yandex.ru>
2015-04-06 10:58:14 +0300

    File update

--- admin.php
+++ admin.php
@@ -11,12 +11,19 @@ if (empty($_SESSION['mail'])){
 
 if (isset($_POST))
 {
+    //TODO: AI XSS issue #47, Medium, Cross-site Scripting, https://github.com/userwithrepo/ACSI_S4/issues/47, null
     echo str_replace($_POST['v'], "\'", "");
+    //TODO: AI XSS issue #48, Medium, Cross-site Scripting, https://github.com/userwithrepo/ACSI_S4/issues/48, null
     echo $_POST['a'];
+    //TODO: AI XSS issue #49, Medium, Cross-site Scripting, https://github.com/userwithrepo/ACSI_S4/issues/49, null
     echo $_POST['b'];
+    //TODO: AI XSS issue #50, Medium, Cross-site Scripting, https://github.com/userwithrepo/ACSI_S4/issues/50, null
     echo $_POST['c'];
+    //TODO: AI XSS issue #51, Medium, Cross-site Scripting, https://github.com/userwithrepo/ACSI_S4/issues/51, null
     echo $_POST['d'];
+    //TODO: AI XSS issue #52, Medium, Cross-site Scripting, https://github.com/userwithrepo/ACSI_S4/issues/52, null
     echo $_POST['e'];
+    //TODO: AI XSS issue #53, Medium, Cross-site Scripting, https://github.com/userwithrepo/ACSI_S4/issues/53, null
     echo $_POST['f'];
 }
 
