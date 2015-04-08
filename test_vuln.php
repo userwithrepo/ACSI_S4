9015e1e1106494fb125b82940b00dcac2242a69a
userwithrepo <bugiz300@yandex.com>
2015-04-08 17:13:28 +0300

    Update test_vuln.php

--- test_vuln.php
+++ test_vuln.php
@@ -1,5 +1,5 @@
 <?php
-TODO: AI AFR issue #1, High, Arbitrary File Reading, https://github.com/userwithrepo/ACSI_S4/issues/1, 2639d1d0
+//TODO: AI AFR issue #1, High, Arbitrary File Reading, https://github.com/userwithrepo/ACSI_S4/issues/1, 0cd3baa9
 //AI exploit:
 //POST /../../../../../../PHP_Repos/userwithrepo.ACSI_S4/userwithrepo-ACSI_S4-f254c7b/admin.php HTTP/1.1
 //Host: localhost
@@ -11,7 +11,7 @@ TODO: AI AFR issue #1, High, Arbitrary File Reading, https://github.com/userwith
 //e=%3Cscript%3Ealert%281%29%3C%2Fscript%3E
 echo $_POST['e'];
 
-TODO: AI RFI issue #1, High, Remote File Inclusion, https://github.com/userwithrepo/ACSI_S4/issues/1, 33c2998e
+//TODO: AI RFI issue #1, High, Remote File Inclusion, https://github.com/userwithrepo/ACSI_S4/issues/1, bdcfe85a
 //AI exploit:
 //POST /../../../../../../PHP_Repos/userwithrepo.ACSI_S4/userwithrepo-ACSI_S4-f254c7b/admin.php HTTP/1.1
 //Host: localhost
@@ -23,7 +23,7 @@ TODO: AI RFI issue #1, High, Remote File Inclusion, https://github.com/userwithr
 //e=%3Cscript%3Ealert%281%29%3C%2Fscript%3E
 echo $_POST['e'];
 
-TODO: AI XXE issue #1, High, XML External Entity, https://github.com/userwithrepo/ACSI_S4/issues/1, 19d90cd3
+//TODO: AI XXE issue #1, High, XML External Entity, https://github.com/userwithrepo/ACSI_S4/issues/1, 589f629a
 //AI exploit:
 //POST /../../../../../../PHP_Repos/userwithrepo.ACSI_S4/userwithrepo-ACSI_S4-f254c7b/admin.php HTTP/1.1
 //Host: localhost
@@ -35,7 +35,7 @@ TODO: AI XXE issue #1, High, XML External Entity, https://github.com/userwithrep
 //e=%3Cscript%3Ealert%281%29%3C%2Fscript%3E
 echo $_POST['e'];
 
-TODO: AI XI issue #1, High, XPath Injection, https://github.com/userwithrepo/ACSI_S4/issues/1, 03b3909a
+//TODO: AI XI issue #1, High, XPath Injection, https://github.com/userwithrepo/ACSI_S4/issues/1, 35c856a0
 //AI exploit:
 //POST /../../../../../../PHP_Repos/userwithrepo.ACSI_S4/userwithrepo-ACSI_S4-f254c7b/admin.php HTTP/1.1
 //Host: localhost
@@ -47,7 +47,7 @@ TODO: AI XI issue #1, High, XPath Injection, https://github.com/userwithrepo/ACS
 //e=%3Cscript%3Ealert%281%29%3C%2Fscript%3E
 echo $_POST['e'];
 
-TODO: AI OR issue #1, Medium, Open Redirect, https://github.com/userwithrepo/ACSI_S4/issues/1, 6b761e41
+//TODO: AI OR issue #1, Medium, Open Redirect, https://github.com/userwithrepo/ACSI_S4/issues/1, 286f3008
 //AI exploit:
 //POST /../../../../../../PHP_Repos/userwithrepo.ACSI_S4/userwithrepo-ACSI_S4-f254c7b/admin.php HTTP/1.1
 //Host: localhost
@@ -59,7 +59,7 @@ TODO: AI OR issue #1, Medium, Open Redirect, https://github.com/userwithrepo/ACS
 //e=%3Cscript%3Ealert%281%29%3C%2Fscript%3E
 echo $_POST['e'];
 
-TODO: AI OSC issue #1, High, OS commanding, https://github.com/userwithrepo/ACSI_S4/issues/1, b811f948
+//TODO: AI OSC issue #1, High, OS commanding, https://github.com/userwithrepo/ACSI_S4/issues/1, f673eff6
 //AI exploit:
 //POST /../../../../../../PHP_Repos/userwithrepo.ACSI_S4/userwithrepo-ACSI_S4-f254c7b/admin.php HTTP/1.1
 //Host: localhost
@@ -71,7 +71,7 @@ TODO: AI OSC issue #1, High, OS commanding, https://github.com/userwithrepo/ACSI
 //e=%3Cscript%3Ealert%281%29%3C%2Fscript%3E
 echo $_POST['e'];
 
-TODO: AI LF issue #1, Medium, Log Forging, https://github.com/userwithrepo/ACSI_S4/issues/1, adae3580
+//TODO: AI LF issue #1, Medium, Log Forging, https://github.com/userwithrepo/ACSI_S4/issues/1, cde5b538
 //AI exploit:
 //POST /../../../../../../PHP_Repos/userwithrepo.ACSI_S4/userwithrepo-ACSI_S4-f254c7b/admin.php HTTP/1.1
 //Host: localhost
@@ -83,7 +83,7 @@ TODO: AI LF issue #1, Medium, Log Forging, https://github.com/userwithrepo/ACSI_
 //e=%3Cscript%3Ealert%281%29%3C%2Fscript%3E
 echo $_POST['e'];
 
-TODO: AI LFI issue #1, High, Local File Inclusion, https://github.com/userwithrepo/ACSI_S4/issues/1, dbe1396c
+//TODO: AI LFI issue #1, High, Local File Inclusion, https://github.com/userwithrepo/ACSI_S4/issues/1, 5e7a644e
 //AI exploit:
 //POST /../../../../../../PHP_Repos/userwithrepo.ACSI_S4/userwithrepo-ACSI_S4-f254c7b/admin.php HTTP/1.1
 //Host: localhost
@@ -95,7 +95,7 @@ TODO: AI LFI issue #1, High, Local File Inclusion, https://github.com/userwithre
 //e=%3Cscript%3Ealert%281%29%3C%2Fscript%3E
 echo $_POST['e'];
 
-TODO: AI XSS issue #1, Medium, Cross-site Scripting, https://github.com/userwithrepo/ACSI_S4/issues/1, 859f8f04
+//TODO: AI XSS issue #1, Medium, Cross-site Scripting, https://github.com/userwithrepo/ACSI_S4/issues/1, 0b81375e
 //AI exploit:
 //POST /../../../../../../PHP_Repos/userwithrepo.ACSI_S4/userwithrepo-ACSI_S4-f254c7b/admin.php HTTP/1.1
 //Host: localhost
@@ -107,7 +107,7 @@ TODO: AI XSS issue #1, Medium, Cross-site Scripting, https://github.com/userwith
 //e=%3Cscript%3Ealert%281%29%3C%2Fscript%3E
 echo $_POST['e'];
 
-TODO: AI AFD issue #1, High, Arbitrary File Deletion, https://github.com/userwithrepo/ACSI_S4/issues/1, 17ee5c4f
+//TODO: AI AFD issue #1, High, Arbitrary File Deletion, https://github.com/userwithrepo/ACSI_S4/issues/1, 66798b82
 //AI exploit:
 //POST /../../../../../../PHP_Repos/userwithrepo.ACSI_S4/userwithrepo-ACSI_S4-f254c7b/admin.php HTTP/1.1
 //Host: localhost
@@ -119,7 +119,7 @@ TODO: AI AFD issue #1, High, Arbitrary File Deletion, https://github.com/userwit
 //e=%3Cscript%3Ealert%281%29%3C%2Fscript%3E
 echo $_POST['e'];
 
-TODO: AI HRS issue #1, Medium, HTTP Response Splitting, https://github.com/userwithrepo/ACSI_S4/issues/1, 7352758a
+//TODO: AI HRS issue #1, Medium, HTTP Response Splitting, https://github.com/userwithrepo/ACSI_S4/issues/1, f06d6335
 //AI exploit:
 //POST /../../../../../../PHP_Repos/userwithrepo.ACSI_S4/userwithrepo-ACSI_S4-f254c7b/admin.php HTTP/1.1
 //Host: localhost
@@ -131,7 +131,7 @@ TODO: AI HRS issue #1, Medium, HTTP Response Splitting, https://github.com/userw
 //e=%3Cscript%3Ealert%281%29%3C%2Fscript%3E
 echo $_POST['e'];
 
-TODO: AI UFP issue #1, High, Unrestricted File Upload, https://github.com/userwithrepo/ACSI_S4/issues/1, 4bf273b2
+//TODO: AI UFP issue #1, High, Unrestricted File Upload, https://github.com/userwithrepo/ACSI_S4/issues/1, aacbb1c1
 //AI exploit:
 //POST /../../../../../../PHP_Repos/userwithrepo.ACSI_S4/userwithrepo-ACSI_S4-f254c7b/admin.php HTTP/1.1
 //Host: localhost
@@ -143,7 +143,7 @@ TODO: AI UFP issue #1, High, Unrestricted File Upload, https://github.com/userwi
 //e=%3Cscript%3Ealert%281%29%3C%2Fscript%3E
 echo $_POST['e'];
 
-TODO: AI SSRF issue #1, High, SSRF, https://github.com/userwithrepo/ACSI_S4/issues/1, b1958670
+//TODO: AI SSRF issue #1, High, SSRF, https://github.com/userwithrepo/ACSI_S4/issues/1, a21b881b
 //AI exploit:
 //POST /../../../../../../PHP_Repos/userwithrepo.ACSI_S4/userwithrepo-ACSI_S4-f254c7b/admin.php HTTP/1.1
 //Host: localhost
@@ -155,7 +155,7 @@ TODO: AI SSRF issue #1, High, SSRF, https://github.com/userwithrepo/ACSI_S4/issu
 //e=%3Cscript%3Ealert%281%29%3C%2Fscript%3E
 echo $_POST['e'];
 
-TODO: AI RCE issue #1, High, Remote Code Execution, https://github.com/userwithrepo/ACSI_S4/issues/1, 96304f71
+//TODO: AI RCE issue #1, High, Remote Code Execution, https://github.com/userwithrepo/ACSI_S4/issues/1, 95a392d2
 //AI exploit:
 //POST /../../../../../../PHP_Repos/userwithrepo.ACSI_S4/userwithrepo-ACSI_S4-f254c7b/admin.php HTTP/1.1
 //Host: localhost
@@ -167,7 +167,7 @@ TODO: AI RCE issue #1, High, Remote Code Execution, https://github.com/userwithr
 //e=%3Cscript%3Ealert%281%29%3C%2Fscript%3E
 echo $_POST['e'];
 
-TODO: AI AFM issue #1, High, Arbitrary File Modification, https://github.com/userwithrepo/ACSI_S4/issues/1, 56f4b1fb
+//TODO: AI AFM issue #1, High, Arbitrary File Modification, https://github.com/userwithrepo/ACSI_S4/issues/1, 7403f2f2
 //AI exploit:
 //POST /../../../../../../PHP_Repos/userwithrepo.ACSI_S4/userwithrepo-ACSI_S4-f254c7b/admin.php HTTP/1.1
 //Host: localhost
@@ -179,7 +179,7 @@ TODO: AI AFM issue #1, High, Arbitrary File Modification, https://github.com/use
 //e=%3Cscript%3Ealert%281%29%3C%2Fscript%3E
 echo $_POST['e'];
 
-TODO: AI SQL issue #1, High, SQL Injection, https://github.com/userwithrepo/ACSI_S4/issues/1, 998907cb
+//TODO: AI SQL issue #1, High, SQL Injection, https://github.com/userwithrepo/ACSI_S4/issues/1, b603969d
 //AI exploit:
 //POST /../../../../../../PHP_Repos/userwithrepo.ACSI_S4/userwithrepo-ACSI_S4-f254c7b/admin.php HTTP/1.1
 //Host: localhost
@@ -191,7 +191,7 @@ TODO: AI SQL issue #1, High, SQL Injection, https://github.com/userwithrepo/ACSI
 //e=%3Cscript%3Ealert%281%29%3C%2Fscript%3E
 echo $_POST['e'];
 
-TODO: AI AFC issue #1, High, Arbitrary File Creation, https://github.com/userwithrepo/ACSI_S4/issues/1, 259cff53
+//TODO: AI AFC issue #1, High, Arbitrary File Creation, https://github.com/userwithrepo/ACSI_S4/issues/1, 3cf520f4
 //AI exploit:
 //POST /../../../../../../PHP_Repos/userwithrepo.ACSI_S4/userwithrepo-ACSI_S4-f254c7b/admin.php HTTP/1.1
 //Host: localhost
@@ -203,7 +203,7 @@ TODO: AI AFC issue #1, High, Arbitrary File Creation, https://github.com/userwit
 //e=%3Cscript%3Ealert%281%29%3C%2Fscript%3E
 echo $_POST['e'];
 
-TODO: AI LDAP issue #1, High, LDAP Injection, https://github.com/userwithrepo/ACSI_S4/issues/1, 19a4f75c
+//TODO: AI LDAP issue #1, High, LDAP Injection, https://github.com/userwithrepo/ACSI_S4/issues/1, 29e899fc
 //AI exploit:
 //POST /../../../../../../PHP_Repos/userwithrepo.ACSI_S4/userwithrepo-ACSI_S4-f254c7b/admin.php HTTP/1.1
 //Host: localhost
@@ -215,7 +215,7 @@ TODO: AI LDAP issue #1, High, LDAP Injection, https://github.com/userwithrepo/AC
 //e=%3Cscript%3Ealert%281%29%3C%2Fscript%3E
 echo $_POST['e'];
 
-TODO: AI DUD issue #1, High, Deserialization of Untrusted Data, https://github.com/userwithrepo/ACSI_S4/issues/1, fc212df5
+//TODO: AI DUD issue #1, High, Deserialization of Untrusted Data, https://github.com/userwithrepo/ACSI_S4/issues/1, d22f7e91
 //AI exploit:
 //POST /../../../../../../PHP_Repos/userwithrepo.ACSI_S4/userwithrepo-ACSI_S4-f254c7b/admin.php HTTP/1.1
 //Host: localhost
@@ -226,4 +226,5 @@ TODO: AI DUD issue #1, High, Deserialization of Untrusted Data, https://github.c
 //
 //e=%3Cscript%3Ealert%281%29%3C%2Fscript%3E
 echo $_POST['e'];
+
 ?>
