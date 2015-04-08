8806afd5114739b788a472f80625b0c851f6fa85
userwithrepo <bugiz300@yandex.com>
2015-04-08 15:41:10 +0300

    Update admin.php

--- admin.php
+++ admin.php
@@ -11,15 +11,15 @@ if (empty($_SESSION['mail'])){
 
 if (isset($_POST))
 {
-    //TODO: AI XSS issue #113, Medium, Cross-site Scripting, https://github.com/userwithrepo/ACSI_S4/issues/113, 4d6447bf
+    //VERIFY: AI XSS issue #113, Medium, Cross-site Scripting, https://github.com/userwithrepo/ACSI_S4/issues/113, 4d6447bf
     //AI exploit:
     //POST /../../../../../../PHP_Repos/userwithrepo.ACSI_S4/userwithrepo-ACSI_S4-f254c7b/admin.php HTTP/1.1
     //Host: localhost
     //Accept-Encoding: identity
     //Connection: close
     //Content-Length: 0
-    echo str_replace($_POST['v'], "\"", "");
-    //TODO: AI XSS issue #113, Medium, Cross-site Scripting, https://github.com/userwithrepo/ACSI_S4/issues/113, 02cd44d8
+    echo $_POST['v'];
+    //FP: AI XSS issue #113, Medium, Cross-site Scripting, https://github.com/userwithrepo/ACSI_S4/issues/113, 02cd44d8
     //AI exploit:
     //POST /../../../../../../PHP_Repos/userwithrepo.ACSI_S4/userwithrepo-ACSI_S4-f254c7b/admin.php HTTP/1.1
     //Host: localhost
@@ -84,7 +84,7 @@ if (isset($_POST))
     //Content-Length: 41
     //
     //f=%3Cscript%3Ealert%281%29%3C%2Fscript%3E
-    echo $_POST['f'];
+    echo str_replace($_POST['f'], "\"", "'");
 }
 
 
