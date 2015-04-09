7cbf9d9b921bee98aa9dd652e024706606b9af40
userwithrepo <bugiz300@yandex.com>
2015-04-09 14:59:19 +0300

    Create issues_types.php

---
+++ issues_types.php
@@ -0,0 +1,19 @@
+//TODO: AI AFR issue #1, High, Arbitrary File Reading, https://github.com/userwithrepo/ACSI_S4/issues/1, 568a1fee
+//TODO: AI RFI issue #1, High, Remote File Inclusion, https://github.com/userwithrepo/ACSI_S4/issues/1, 682800b0
+//TODO: AI XXE issue #1, High, XML External Entity, https://github.com/userwithrepo/ACSI_S4/issues/1, 2ecf32c5
+//TODO: AI XI issue #1, High, XPath Injection, https://github.com/userwithrepo/ACSI_S4/issues/1, d898cc9c
+//TODO: AI OR issue #1, Medium, Open Redirect, https://github.com/userwithrepo/ACSI_S4/issues/1, 4fff6c53
+//TODO: AI OSC issue #1, High, OS commanding, https://github.com/userwithrepo/ACSI_S4/issues/1, 6ee6986e
+//TODO: AI LF issue #1, Medium, Log Forging, https://github.com/userwithrepo/ACSI_S4/issues/1, 040ab4d6
+//TODO: AI LFI issue #1, High, Local File Inclusion, https://github.com/userwithrepo/ACSI_S4/issues/1, 48a48459
+//TODO: AI XSS issue #1, Medium, Cross-site Scripting, https://github.com/userwithrepo/ACSI_S4/issues/1, fe57ee7e
+//TODO: AI AFD issue #1, High, Arbitrary File Deletion, https://github.com/userwithrepo/ACSI_S4/issues/1, 201e6231
+//TODO: AI HRS issue #1, Medium, HTTP Response Splitting, https://github.com/userwithrepo/ACSI_S4/issues/1, a5a1b761
+//TODO: AI UFP issue #1, High, Unrestricted File Upload, https://github.com/userwithrepo/ACSI_S4/issues/1, b01f41c7
+//TODO: AI SSRF issue #1, High, SSRF, https://github.com/userwithrepo/ACSI_S4/issues/1, 45ceb3ab
+//TODO: AI RCE issue #1, High, Remote Code Execution, https://github.com/userwithrepo/ACSI_S4/issues/1, 7fb08ceb
+//TODO: AI AFM issue #1, High, Arbitrary File Modification, https://github.com/userwithrepo/ACSI_S4/issues/1, 4a9ffc61
+//TODO: AI SQL issue #1, High, SQL Injection, https://github.com/userwithrepo/ACSI_S4/issues/1, 73acd3c0
+//TODO: AI AFC issue #1, High, Arbitrary File Creation, https://github.com/userwithrepo/ACSI_S4/issues/1, c9102a5b
+//TODO: AI LDAP issue #1, High, LDAP Injection, https://github.com/userwithrepo/ACSI_S4/issues/1, 36042177
+//TODO: AI DUD issue #1, High, Deserialization of Untrusted Data, https://github.com/userwithrepo/ACSI_S4/issues/1, c1d583b5
