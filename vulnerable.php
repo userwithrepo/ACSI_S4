2ecc77398d7e4980b55792e311c778e8a4f0cc69
userwithrepo <bugiz300@yandex.com>
2015-04-13 19:36:34 +0300

    Update vulnerable.php

--- vulnerable.php
+++ vulnerable.php
@@ -1,4 +1,13 @@
 <?php
 echo $_REQUEST['id'];
-
+$id = $_REQUEST['id'];
+$user_id = $_REQUEST['user_id'];
+echo "Message for user_id = $user_id and message_id = $message_id";
+if(isset($id) && isset($user_id))
+{
+	$res = mysql_query("SELECT * FROM messages WHERE msg_id = '$id' AND user_id = '$user_id'") or die(mysql_error());
+	$res = mysql_fetch_assoc($res);
+	echo "Message for user_id = $user_id and message_id = $message_id";
+	echo "<div>$res[body]</div>";
+}
 ?>
