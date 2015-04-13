1f6ea6dadf3b18970e825aef278610d81585e165
userwithrepo <bugiz300@yandex.com>
2015-04-13 20:02:58 +0300

    Update vulnerable.php

--- vulnerable.php
+++ vulnerable.php
@@ -1,7 +1,7 @@
 <?php
 $id = htmlspecialchars(mysql_real_escape_string($_REQUEST['id']));
 $user_id = $_REQUEST['user_id'];
-//TODO: AI XSS issue #155, Medium, Cross-site Scripting, https://github.com/userwithrepo/ACSI_S4/issues/155, ad2a0e0b
+//VERIFY: AI XSS issue #155, Medium, Cross-site Scripting, https://github.com/userwithrepo/ACSI_S4/issues/155, ad2a0e0b
 //AI exploit:
 //GET /../../../../../../PHP_Repos/userwithrepo.ACSI_S4/userwithrepo-ACSI_S4-c2ef4b4/vulnerable.php?user_id=%3Cscript%3Ealert%281%29%3C%2Fscript%3E HTTP/1.1
 //Host: localhost
@@ -18,14 +18,14 @@ if(isset($id) && isset($user_id))
 	//Connection: close
 	$res = mysql_query("SELECT * FROM messages WHERE msg_id = '$id'") or die(mysql_error());
 	$res = mysql_fetch_assoc($res);
-	//TODO: AI XSS issue #155, Medium, Cross-site Scripting, https://github.com/userwithrepo/ACSI_S4/issues/155, 05f7e6a3
+	//VERIFY: AI XSS issue #155, Medium, Cross-site Scripting, https://github.com/userwithrepo/ACSI_S4/issues/155, 05f7e6a3
 	//AI exploit:
 	//GET /../../../../../../PHP_Repos/userwithrepo.ACSI_S4/userwithrepo-ACSI_S4-c2ef4b4/vulnerable.php?user_id=%3Cscript%3Ealert%281%29%3C%2Fscript%3E&id=1 HTTP/1.1
 	//Host: localhost
 	//Accept-Encoding: identity
 	//Connection: close
 	echo "Message for user_id = $user_id and message_id = $message_id";
-	//TODO: AI XSS issue #155, Medium, Cross-site Scripting, https://github.com/userwithrepo/ACSI_S4/issues/155, 0768da97
+	//FP: AI XSS issue #155, Medium, Cross-site Scripting, https://github.com/userwithrepo/ACSI_S4/issues/155, 0768da97
 	//AI exploit:
 	//GET /../../../../../../PHP_Repos/userwithrepo.ACSI_S4/userwithrepo-ACSI_S4-c2ef4b4/vulnerable.php?user_id=1&id=1 HTTP/1.1
 	//Host: localhost
