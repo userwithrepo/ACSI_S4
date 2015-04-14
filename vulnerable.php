6e95070ee6bcd0f7f99a6f81b622f7fa0cd34fa5
bugisup <bugiz300@yandex.ru>
2015-04-14 10:45:23 +0300

    File update

--- vulnerable.php
+++ vulnerable.php
@@ -11,5 +11,11 @@
 //Host: localhost
 //Accept-Encoding: identity
 //Connection: close
+//TODO: AI XSS issue #189, Medium, Cross-site Scripting, https://github.com/userwithrepo/ACSI_S4/issues/189, 456dbc10
+//AI exploit:
+//GET /../../../../../../PHP_Repos/userwithrepo.ACSI_S4/userwithrepo-ACSI_S4-d7244f6/vulnerable.php?id2=%3Cscript%3Ealert%281%29%3C%2Fscript%3E HTTP/1.1
+//Host: localhost
+//Accept-Encoding: identity
+//Connection: close
 echo $_GET['id1']." ".$_GET['id2'];
 ?>
