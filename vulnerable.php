6b1e1446690893e23742a3ae4e6cf838c51a00df
bugisup <bugiz300@yandex.ru>
2015-05-28 18:36:40 +0400

    Update vulnerable.php

--- vulnerable.php
+++ vulnerable.php
@@ -3,6 +3,7 @@ $id = $_GET['id'];
 $user_id = $_GET['user_id'];
 if(isset($id) && isset($user_id))
 {
+    // XSS 1
     echo "Dialog: $id";
     $res = mysql_query("SELECT msg FROM dialogs WHERE user_id='$user_id'") or dir(mysql_error());
     $res = mysql_fetch_assoc($res);
